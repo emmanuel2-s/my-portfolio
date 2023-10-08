264c711baff5a633b0a2dce44f371bc5efae6505
emmanuel2-s <cosmasemmanuel40@gmail.com>
2023-10-08 13:09:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-extrabold text-4xl text-[#00df9a]'>l am emmanuel</h1>
+        <button className='bg-black text-[#00df9a] rounded font-medium px-5 py-2'>click me</button>
+      </div>
+    </>
+  )
+}
+
+export default App
